8875988c9277b700cf1e1594a6da2c9a5f17eb70
rummolprod999 <rummolprod999@gmail.com>
2019-02-22 12:40:17 +0300

    refractoring

--- ClassProtocolCancel.py
+++ ClassProtocolCancel.py
@@ -1,8 +1,12 @@
+import datetime
+
 import dateutil.parser
 import pytz
 
 import parser_prot
 from ClassProtocol import Protocol
+from connect_to_db import connect_bd
+from parser_prot import logging_parser, DB, PREFIX
 
 
 class ProtocolCancel(Protocol):
@@ -45,3 +49,60 @@ class ProtocolCancel(Protocol):
                 self.protocol, 'cancelReason',
                 'courtDecision', 'docName')
         return d
+
+
+def parserCancel(doc, path_xml, filexml, reg, type_f):
+    p = ProtocolCancel(doc, filexml)
+    purchase_number = p.get_purchaseNumber()
+    # print(purchase_number)
+    if not purchase_number:
+        logging_parser('У протокола нет purchase_number', path_xml)
+        return
+    xml = path_xml[path_xml.find('/') + 1:][(path_xml[path_xml.find('/') + 1:]).find('/') + 1:]
+    id_protocol = p.get_id()
+    protocol_number = p.get_protocol_number()
+    url = p.get_url()
+    print_form = p.get_print_form()
+    protocol_date = p.get_protocol_date()
+    authority_name = p.get_authority_name()
+    doc_name = p.get_doc_name()
+    con = connect_bd(DB)
+    cur = con.cursor()
+    cur.execute(f"""SELECT id FROM {PREFIX}auction_protocol_cancel WHERE id_protocol = %s AND purchase_number = %s 
+                        AND protocol_number = %s""",
+                (id_protocol, purchase_number, protocol_number))
+    res_id = cur.fetchone()
+    if res_id:
+        # logging_parser('такой протокол есть в базе', xml)
+        cur.close()
+        con.close()
+        return
+    cancel_status = 0
+    updated = False
+    date_prot = dateutil.parser.parse(protocol_date[:19])
+    cur.execute(
+            f"""SELECT id, protocol_date FROM {PREFIX}auction_protocol_cancel WHERE purchase_number = %s AND protocol_number = %s""",
+            (purchase_number, protocol_number))
+    res_prot = cur.fetchall()
+    if res_prot:
+        updated = True
+        for r in res_prot:
+            if date_prot > datetime.datetime.strptime(str(r['protocol_date']), "%Y-%m-%d %H:%M:%S"):
+                cur.execute(f"""UPDATE {PREFIX}auction_start_protocol SET cancel = 1 WHERE id = %s""",
+                            (r['id'],))
+            else:
+                cancel_status = 1
+    cur.execute(
+            f"""INSERT INTO {PREFIX}auction_protocol_cancel SET id_protocol = %s, protocol_date =  %s, purchase_number = %s, 
+                        url = %s, print_form = %s, xml = %s, protocol_number = %s, cancel = %s, authority_name = %s, doc_name = %s""",
+            (id_protocol, protocol_date, purchase_number, url, print_form, xml, protocol_number, cancel_status,
+             authority_name, doc_name))
+    id_p = cur.lastrowid
+    if not id_p:
+        logging_parser('Не получили id', xml)
+    if updated:
+        ProtocolCancel.update_protocolCancel += 1
+    else:
+        ProtocolCancel.add_protocolCancel += 1
+    cur.close()
+    con.close()
--- ClassProtocolEOK1.py
+++ ClassProtocolEOK1.py
@@ -1,4 +1,10 @@
+import datetime
+
+import dateutil.parser
+
 from ClassProtocolEOK2 import ProtocolEOK2
+from connect_to_db import connect_bd
+from parser_prot import logging_parser, DB, PREFIX
 
 
 class ProtocolEOK1(ProtocolEOK2):
@@ -7,3 +13,88 @@ class ProtocolEOK1(ProtocolEOK2):
 
     def __init__(self, protocol, xml):
         super().__init__(protocol, xml)
+
+
+def parserEOK1(doc, path_xml, filexml, reg, type_f):
+    p = ProtocolEOK1(doc, filexml)
+    purchase_number = p.get_purchaseNumber()
+    # print(purchase_number)
+    if not purchase_number:
+        logging_parser('У протокола нет purchase_number', path_xml)
+        return
+    xml = path_xml[path_xml.find('/') + 1:][(path_xml[path_xml.find('/') + 1:]).find('/') + 1:]
+    id_protocol = p.get_id()
+    url = p.get_url()
+    print_form = p.get_print_form()
+    protocol_date = p.get_protocol_date()
+    con = connect_bd(DB)
+    cur = con.cursor()
+    lot_number = 1
+    abandoned_reason_name = p.get_abandoned_reason_name()
+    cur.execute(
+            f"""SELECT id FROM {PREFIX}auction_start_protocol WHERE id_protocol = %s AND purchase_number = %s 
+            AND type_protocol = %s AND lot_number = %s""",
+            (id_protocol, purchase_number, type_f, lot_number))
+    res_id = cur.fetchone()
+    if res_id:
+        # logging_parser('такой протокол есть в базе', xml)
+        cur.close()
+        con.close()
+        return
+    cancel_status = 0
+    updated = False
+    date_prot = dateutil.parser.parse(protocol_date[:19])
+    cur.execute(f"""SELECT id, protocol_date FROM {PREFIX}auction_start_protocol WHERE purchase_number = %s 
+                                   AND type_protocol = %s AND lot_number = %s""",
+                (purchase_number, type_f, lot_number))
+    res_prot = cur.fetchall()
+    if res_prot:
+        updated = True
+        for r in res_prot:
+            if date_prot >= datetime.datetime.strptime(str(r['protocol_date']), "%Y-%m-%d %H:%M:%S"):
+                cur.execute(f"""UPDATE {PREFIX}auction_start_protocol SET cancel = 1 WHERE id = %s""",
+                            (r['id'],))
+            else:
+                cancel_status = 1
+    cur.execute(
+            f"""INSERT INTO {PREFIX}auction_start_protocol SET id_protocol = %s, protocol_date =  %s, purchase_number = %s, 
+                                       url = %s, print_form = %s, xml = %s, type_protocol = %s, cancel = %s, abandoned_reason_name = %s, 
+                                       lot_number = %s""",
+            (id_protocol, protocol_date, purchase_number, url, print_form, xml, type_f, cancel_status,
+             abandoned_reason_name, lot_number))
+    id_p = cur.lastrowid
+    if not id_p:
+        logging_parser('Не получили id', xml)
+    if updated:
+        ProtocolEOK1.update_protocolEOK1 += 1
+    else:
+        ProtocolEOK1.add_protocolEOK1 += 1
+    for app in p.applications:
+        journal_number = p.get_journal_number(app)
+        admission = p.get_admission(app)
+        id_participiant = 0
+        inn = p.get_inn(app)
+        kpp = p.get_kpp(app)
+        organization_name = p.get_organization_name(app)
+        participant_type = p.get_participant_type(app)
+        country_full_name = p.get_country_full_name(app)
+        post_address = p.get_post_address(app)
+        if inn:
+            cur.execute(f"""SELECT id FROM {PREFIX}auction_participant WHERE inn = %s AND kpp = %s""",
+                        (inn, kpp))
+            res_p = cur.fetchone()
+            if res_p:
+                id_participiant = res_p['id']
+            else:
+                cur.execute(f"""INSERT INTO {PREFIX}auction_participant SET inn = %s, kpp = %s, 
+                                                   organization_name = %s, participant_type = %s, country_full_name = %s, 
+                                                   post_address = %s""",
+                            (inn, kpp, organization_name, participant_type, country_full_name, post_address))
+                id_participiant = cur.lastrowid
+
+        cur.execute(f"""INSERT INTO {PREFIX}auction_start_applications SET id_auction_protocol = %s, 
+                                                   journal_number = %s, admission = %s,
+                                                   id_participiant = %s""",
+                    (id_p, journal_number, admission, id_participiant))
+    cur.close()
+    con.close()
